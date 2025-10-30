2ce9a5a0996fbc08b4143b27c444c4e2544b3abd
ainezeinstein10-del <ainez_einstein@yahoo.com>
2025-10-31 00:41:43 +0800

    Add dummy Cypress test for CI

--- cypress/e2e/ci-test.cy.js
+++ cypress/e2e/ci-test.cy.js
@@ -1,6 +1,15 @@
-// cypress/e2e/ci-test.cy.js
-describe('CI Workflow Test', () => {
-  it('runs successfully without a server', () => {
+
     expect(true).to.equal(true)
   })
 })
+
+
+
+
+
+
+
+
+
+
+
